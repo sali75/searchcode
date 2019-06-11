a0d5378a8f636f0530c9992fe7c617e14a4a2bce
sali75 <sali75911@gmail.com>
2019-06-11 22:34:34 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import users from "./data/users";
 
-function App() {
+
+class App extends Component{
+
+  state = { users: users };
+
+search = () => {
+    alert("hi")
+  }
+
+  render()
+  {
+      const { users } = this.state;
+      console.log(users);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App row">
+      <div className="bg-info col-12 d-flex justify-content-center align-items-center py-100" style={{"height":"200px"}}>
+        <Form inline>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+              <Button onClick={this.search} color="primary">جستجو</Button>
+            </FormGroup>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+              <Input type="email" dir="rtl" name="email" id="exampleEmail" placeholder="کد ملی را وارد کنید" />
+              <Label for="exampleEmail" className="mr-sm-2 ml-sm-1 text-white">:کدملی</Label>
+          </FormGroup>
+        </Form>
+      </div>
+      <div className="col-8 bg-primary">
+        bye
+      </div>
+      <div className="col-4 bg-warning">
+        side
+      </div>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/data/users.js
@@ -0,0 +1,44 @@
+const users = [
+  {
+    id: 0,
+    name: "ali",
+    lastName: "sadeghi",
+    code: "117075876",
+    accountNumber: "6037997333111135",
+    balance: 120000
+  },
+  {
+    id: 1,
+    name: "saeid",
+    lastName: "amini",
+    code: "1160749076",
+    accountNumber: "037997333020136",
+    balance: 7000000
+  },
+  {
+    id: 1,
+    name: "hamid",
+    lastName: "javan",
+    code: "1170721023",
+    accountNumber: "037997333550137",
+    balance: 1500000
+  },
+  {
+    id: 2,
+    name: "reza",
+    lastName: "samadi",
+    code: "1170742056",
+    accountNumber: "037997333058138",
+    balance: 250000
+  },
+  {
+    id: 4,
+    name: "vahid",
+    lastName: "mohamadi",
+    code: "1170965079",
+    accountNumber: "037997333000139",
+    balance: 50000
+  }
+];
+
+export default users;
